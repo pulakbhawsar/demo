6130a59e2d4e699799e3448adeb884021b95deb3
EMPOWER <EMPOWER@EMPOWER110>
2018-03-07 16:04:16 +0530

    Add project files.

---
+++ DemoWebApp/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoWebApp.Startup))]
+namespace DemoWebApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
